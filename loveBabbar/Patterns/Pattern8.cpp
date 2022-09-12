e166b0ee3abf3e44f56ab90321c2c49442eedf7b
kise <kaiazmi1412@gmail.com>
2022-09-12 16:47:35 +0530

    :zap: Pattern 8 Output added

--- loveBabbar/Patterns/Pattern8.cpp
+++ loveBabbar/Patterns/Pattern8.cpp
@@ -8,7 +8,13 @@ int main() {
 
     int row = 1;
     int count = 1;
-
+    /* Output: 5
+    1 
+    2 3 
+    4 5 6 
+    7 8 9 10 
+    11 12 13 14 15 
+    */
     while(row <= n) {
 
         int col = 1;
