78ef3f2f484a55b1c6347d76b71733a4ba0171c9
kise <kaiazmi1412@gmail.com>
2022-09-20 05:49:03 +0530

    :sparkles: L\R Shifting Operator!

--- loveBabbar/BitwiseOperator.cpp
+++ loveBabbar/BitwiseOperator.cpp
@@ -9,4 +9,11 @@ int main() {
     cout << " a|b: " << (a|b) << endl;
     cout << " ~a: " << ~a << endl;
     cout << " a^b: " << (a^b) << endl;
+
+    // Left | Right Shifting:
+
+    cout << (17 >> 1) << endl; // R
+    cout << (17 >> 2) << endl; // R
+    cout << (19 << 1) << endl; // L
+    cout << (21 << 2) << endl; // L
 }
