2851cf30561ff2bdb976de2591266b9c2cfccb22
kise <kaiazmi1412@gmail.com>
2022-12-23 18:48:49 +0530

    :sparkles: datatype

---
+++ C++/dataType.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    char grade = 'A'; // char - dataType uses 'A' single quotes
+    string phrase = "Giraffe Academy";
+    int age = 50;
+    double gpa = 2.3; // "float"/"double" - this mostly used
+    bool isMale = false;
+
+
+    return 0;
+}
