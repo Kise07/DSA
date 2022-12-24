ee8289a666edda79b2353bc74f28427d2b677d00
kise <kaiazmi1412@gmail.com>
2022-12-24 15:58:18 +0530

    :sparkles: Working with Numbers

---
+++ C++/numbers.cpp
@@ -0,0 +1,19 @@
+// Working with Numbers
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    // Math Functions: -
+    cout << pow(2, 5) << endl;; // power
+    cout << sqrt(49) << endl;; // squareroot
+    cout << round(4.3) << endl; // round no.
+    cout << ceil(4.1) << endl; // highest no.
+    cout << floor(4.8) << endl; // lower no.
+    cout << fmax(3, 10) << endl; // max no.
+    cout << fmin(3, 10) << endl; // min no.
+
+    return 0;
+}
