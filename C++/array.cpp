94a86d93ccf73e5328afa5f9099949e4bd3cbdba
kise <kaiazmi1412@gmail.com>
2022-12-25 13:45:57 +0530

    :sparkles: Arrays!

---
+++ C++/array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // 1-method
+    // int luckyNums[] = {4, 8, 15, 16, 23, 42}; // Arrays can be predefined or define later
+    
+    // 2-method
+    int luckyNums[20]; // giving size of array for later defining
+
+    luckyNums[0] = 100;
+
+    cout << luckyNums[0];
+
+    return 0;
+}
