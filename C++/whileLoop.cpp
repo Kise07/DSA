7794c25c56eeee85bf9458769c54f952e4e68c21
kise <kaiazmi1412@gmail.com>
2022-12-28 23:49:57 +0530

    :sparkles: while Loop (do-while Loop)

---
+++ C++/whileLoop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // whileLoop:-
+    // int index = 1;
+    // while(index <= 5) {
+    //     cout << index << endl;
+    //     index++;
+    // }
+
+    // do-whileLoop:-
+    int index = 1;
+    do {
+        cout << index << endl;
+        index++;
+    } while(index <= 5);
+
+    return 0;
+}
