a409b909629ef269d63b25aca48f3de2ba94e1ec
kise <kaiazmi1412@gmail.com>
2022-12-29 15:16:00 +0530

    :sparkles: for Loop "Ex. Arrays iteration"

--- C++/forLoop.cpp
+++ C++/forLoop.cpp
@@ -3,8 +3,10 @@ using namespace std;
 
 int main() {
 
-    for(int i = 1; i <= 5; i++) {
-        cout << i << endl;
+    // Example: forLoop in Arrays:
+    int nums[] = {1, 2, 5, 7, 3};
+    for(int i = 0; i < 5; i++) {
+        cout << nums[i] << endl;
     }
 
     return 0;
