57bca8a0e12738378e710918a27da90e434fa3d8
kise <kaiazmi1412@gmail.com>
2022-12-29 13:11:32 +0530

    :tada: Building a Guessing Game {v2}

--- C++/C++Projects/GuessingGame.cpp
+++ C++/C++Projects/GuessingGame.cpp
@@ -5,13 +5,25 @@ int main() {
 
     int secretNum = 7;
     int guess;
+    int guessCount = 0;
+    int guessLimit = 3;
+    bool outOfGuesses = false;
 
-    while(secretNum != guess) {
-        cout << "Enter guess: ";
-        cin >> guess;
+    while(secretNum != guess && !outOfGuesses) {
+        if(guessCount < guessLimit) {
+            cout << "Enter guess: ";
+            cin >> guess;
+            guessCount++;
+        } else {
+            outOfGuesses = true;
+        }
     }
 
-    cout << "You Win!";
+    if(outOfGuesses) {
+        cout << "You Lose!";
+    } else {
+        cout << "You Win!";
+    }
 
     return 0;
 }
