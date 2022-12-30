ec325f624c7cc5274db495327c1c5fa27d33b3d7
kise <kaiazmi1412@gmail.com>
2022-12-30 21:52:12 +0530

    :sparkles: 2DArray

---
+++ C++/2DArray.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 2DArray:-
+    int numberGrid[3][2] = {
+                            {1, 2},
+                            {3, 4},
+                            {5, 6}
+                        };
+    cout << numberGrid[2][0]; // Output - 5
+
+    return 0;
+}
