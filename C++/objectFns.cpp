64099d15a8152b04dea9584f3348a2fe8ba4a6a7
kise <kaiazmi1412@gmail.com>
2022-12-31 15:00:25 +0530

    :sparkles: Object Functions()!

---
+++ C++/objectFns.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+    public:
+        string name;
+        string major;
+        double gpa;
+        Student(string aName, string aMajor, double aGpa) {
+            name = aName;
+            major = aMajor;
+            gpa = aGpa;
+        }
+        // Object Functions: datatype funcName() {conditions}
+        bool hasHonors() {
+            if(gpa >= 2.0) { // Object func can be modified like 3.5 to 2.0
+                return true;
+            }
+            return false;
+        }
+};
+
+int main() {
+
+    Student student1("Jim", "Business", 2.4);
+    Student student2("Pam", "Art", 3.6);
+
+    cout << student1.hasHonors();
+
+    return 0;
+}
