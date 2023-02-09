c8bba8eab09a6090d145015382849d49b7825201
kise <kaiazmi1412@gmail.com>
2023-02-10 00:06:59 +0530

    :bookmark: Pattern 23 done!

---
+++ loveBabbar/Patterns/Pattern23.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	// Outer loop
+	int i = 1;
+	while(i <= n) {
+
+		// Inner loop
+		int j = n, space = 1, count = i;
+
+		// Space print
+		while(space <= i - 1) {
+
+			cout << "  ";
+			space = space + 1;
+		}
+
+		// Stars print
+		while(j >= i) {
+
+			cout << count << " ";
+			count = count + 1;
+			j = j - 1;
+		}
+		cout << endl;
+		i = i + 1;
+	}
+}
